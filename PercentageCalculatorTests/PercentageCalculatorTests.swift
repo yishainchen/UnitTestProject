e1c2da69410f0907a791efc156897d108bee3b2c
B1media <chenyishain@gmail.com>
2016-06-27 11:29:29 +0800

    first test success

--- PercentageCalculatorTests/PercentageCalculatorTests.swift
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -11,29 +11,38 @@ import XCTest
 
 class PercentageCalculatorTests: XCTestCase {
     
+    var vc: ViewController!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        
-        
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        vc = storyboard.instantiateInitialViewController() as! ViewController
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+  
+    func testPercentageCalculator() {
+        // Should be 25
+        let p = vc.percentage(50, 50)
+        XCTAssert(p == 25)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
+    
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
